881a2131390cec3906ab0329c0d68cb4f871eb18
raginiSahu11 <ragini.sahu@icloud.com>
2025-09-06 17:53:26 +0530

    [Ragini] add : added Friends.java

---
+++ Core_java/Arrays/Level2/Friends.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Friends {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String[] names = {"Amar", "Akbar", "Anthony"};
+        int[] ages = new int[3];
+        int[] heights = new int[3];
+
+        for (int i = 0; i < 3; i++) {
+            System.out.println("Enter age and height of " + names[i] + ": ");
+            ages[i] = sc.nextInt();
+            heights[i] = sc.nextInt();
+        }
+
+        int youngestIndex = 0, tallestIndex = 0;
+        for (int i = 1; i < 3; i++) {
+            if (ages[i] < ages[youngestIndex]) youngestIndex = i;
+            if (heights[i] > heights[tallestIndex]) tallestIndex = i;
+        }
+
+        System.out.println("Youngest friend is " + names[youngestIndex]);
+        System.out.println("Tallest friend is " + names[tallestIndex]);
+        sc.close();
+    }
+}

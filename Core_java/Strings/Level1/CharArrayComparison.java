1206a7ffae3c1efffcc21a1b300aad3e9a9b0502
raginiSahu11 <ragini.sahu@icloud.com>
2025-09-06 18:15:12 +0530

    [Ragini] add : added CharArrayComparison.java

---
+++ Core_java/Strings/Level1/CharArrayComparison.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class CharArrayComparison {
+    public static char[] customToCharArray(String str) {
+        char[] arr = new char[str.length()];
+        for (int i = 0; i < str.length(); i++) {
+            arr[i] = str.charAt(i);
+        }
+        return arr;
+    }
+
+    public static boolean compareArrays(char[] a1, char[] a2) {
+        if (a1.length != a2.length) return false;
+        for (int i = 0; i < a1.length; i++) {
+            if (a1[i] != a2[i]) return false;
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter text: ");
+        String text = sc.next();
+
+        char[] arr1 = customToCharArray(text);
+        char[] arr2 = text.toCharArray();
+
+        System.out.println("Comparison Result: " + compareArrays(arr1, arr2));
+
+        sc.close();
+    }
+}

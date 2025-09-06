3e246d334ed1c169bb1684263a37424a71e7a209
raginiSahu11 <ragini.sahu@icloud.com>
2025-09-06 18:31:10 +0530

    [Ragini] add : added VowelConsonantCount.java

---
+++ Core_java/Strings/Level2/VowelConsonantCount.java
@@ -0,0 +1,30 @@
+import java.util.*;
+
+public class VowelConsonantCount {
+    public static boolean isVowel(char ch) {
+        ch = Character.toLowerCase(ch);
+        return "aeiou".indexOf(ch) != -1;
+    }
+
+    public static int[] countVowelsConsonants(String text) {
+        int vowels = 0, consonants = 0;
+        for (int i = 0; i < text.length(); i++) {
+            char ch = text.charAt(i);
+            if (Character.isLetter(ch)) {
+                if (isVowel(ch)) vowels++;
+                else consonants++;
+            }
+        }
+        return new int[]{vowels, consonants};
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter text: ");
+        String text = sc.nextLine();
+
+        int[] counts = countVowelsConsonants(text);
+        System.out.println("Vowels: " + counts[0]);
+        System.out.println("Consonants: " + counts[1]);
+    }
+}

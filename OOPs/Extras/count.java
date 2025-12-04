e10384a9f540f5d96e483c2d50554b6e14e9cf73
raginiSahu11 <ragini.sahu@icloud.com>
2025-12-04 11:47:45 +0530

    [Ragini] add : added count.java

---
+++ OOPs/Extras/count.java
@@ -0,0 +1,26 @@
+class VowelConsonantCount {
+
+    static void count(String s) {
+        int vowels = 0, consonants = 0;
+        s = s.toLowerCase();
+
+        for (int i = 0; i < s.length(); i++) {
+            char ch = s.charAt(i);
+
+            if (ch >= 'a' && ch <= 'z') {
+                if (ch == 'a' || ch == 'e' || ch == 'i' || 
+                    ch == 'o' || ch == 'u')
+                    vowels++;
+                else
+                    consonants++;
+            }
+        }
+
+        System.out.println("Vowels: " + vowels);
+        System.out.println("Consonants: " + consonants);
+    }
+
+    public static void main(String[] args) {
+        count("Hello World");
+    }
+}
